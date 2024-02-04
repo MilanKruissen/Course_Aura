eff1aac564293c0c9fb415862e424850b5df634a
MilanKruissen <Milankruissen13@gmail.com>
2024-02-04 16:29:38 +0100

    ReadyForCMM

--- Aura/Aura_UE_Version_5_3/Aura/Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Aura/Aura_UE_Version_5_3/Aura/Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,6 +3,7 @@
 #include "Player/AuraPlayerState.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
 AAuraPlayerState::AAuraPlayerState()
 {
@@ -13,6 +14,18 @@ AAuraPlayerState::AAuraPlayerState()
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 	
 	NetUpdateFrequency = 100.f;
+}
+
+void AAuraPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AAuraPlayerState, Level);
+}
+
+void AAuraPlayerState::OnRep_Level(int32 OldLevel)
+{
+	
 }
 
 UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
